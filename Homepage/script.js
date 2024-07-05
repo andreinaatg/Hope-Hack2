789a19112f52f29a3e0f942d2cb4f2ac5d573a58
BeniKiambote10 <benikiambote10@gmail.com>
2024-07-05 11:43:25 -0400

    made changes to the about page and homepage

---
+++ Homepage/script.js
@@ -0,0 +1,10 @@
+window.aichatbotApiKey = "dfa2910e-2ef2-42a2-b2ef-5dbec8fd78dd";
+window.aichatbotProviderId = "f9e9c5e4-6d1a-4b8c-8d3f-3f9e9c5e46d1";
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://script.chatlab.com/aichatbot.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'dfa2910e-2ef2-42a2-b2ef-5dbec8fd78dd'));
